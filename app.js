c825b224a9ab117157d56042a3a81936905f8e88
Ptiga <tylerdurdenxp@gmail.com>
2022-11-02 10:20:35 +0100

    modification

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const port = 5022
 
 app.get('/', (req, res) => {
     res.set('Content-Type', 'text/html');
-    res.send('Si tu vois ce message, tu as réussi !!')
+    res.send('Si tu vois ce message, tu as réussi !!\nEt en plus, il y a une MàJ !!!')
 });
 
 app.listen(port, '0.0.0.0', () => {
